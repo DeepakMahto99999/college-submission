5a7fc97fb233f08975fa83844b8e5fb5e4181d26
DeepakMahto99999 <mahtodeepak239@gmail.com>
2026-03-31 09:53:34 +0530

    backend deployment error

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/*", cors(corsOptions));
 
 app.use(requestLogger)
 app.use("/api", apiLimiter);
